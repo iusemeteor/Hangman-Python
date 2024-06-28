353651c964acc6c5a675bff761ff096af9f18ede
meteor <catboyshadow87@gmail.com>
2024-06-28 17:59:27 -0500

    fixed configuration.py

--- utils/configuration.py
+++ utils/configuration.py
@@ -1,53 +1,23 @@
 import yaml
 
-def get_token():
-    try:
-        with open('config.yml', 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['token']
-    except FileNotFoundError:
-        return None
 
-def getGuildID():
+def getCheat():
     try:
         with open('config.yml', 'r') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['guild_id']
-    except FileNotFoundError:
-        return None
-    
-def get_characterai_token():
-    try:
-        with open('config.yml', 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['character_ai_token']
-    except FileNotFoundError:
-        return None
-
-def get_characterai_character_id():
-    try:
-        with open('config.yml', 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['character_ai_character_id']
-    except FileNotFoundError:
-        return None
-
-def get_dms_only():
-    try:
-        with open('config.yml', 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['dms_only']
+            return config['CHEAT']
     except FileNotFoundError:
         return None
     except KeyError:
-        return False  # Default to False if the key is missing
+        print("In config.yml, please set ('CHEAT') to True or False (case sensitive).")
 
-def getDebug():
+
+def getImpossible():
     try:
         with open('config.yml', 'r') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-            return config['debug']
+            return config['IMPOSSIBLE']
     except FileNotFoundError:
         return None
     except KeyError:
-        return False  # Default to False if the key is missing
+        print("In config.yml, please set ('IMPOSSIBLE') to True or False (case sensitive).")
